fe3f13e95731c8f20fbaee97864dacbae544c0ad
寂寞侯 <2877096614@qq.com>
2021-08-18 16:15:24 +0800

    third

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 from app import app  # 从app包中导入 app这个实例
 
@@ -21,3 +22,12 @@ def index():
         }
     ]
     return render_template('index.html', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()  # 表单实例化对象
+    if login_form.validate_on_submit():
+        flash('Login requested for user {},remember_me={}'.format(login_form.username.data, login_form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=login_form)
