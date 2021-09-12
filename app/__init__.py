cdb1c9eb0f7abde7aec78392cd97ba02c4b75c0c
寂寞侯 <2877096614@qq.com>
2021-09-12 19:31:45 +0800

    eighth

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy  # 从包中导入类
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from logging.handlers import RotatingFileHandler
-import os
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)  # 将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 app.config.from_object(Config)
@@ -12,6 +13,8 @@ db = SQLAlchemy(app)  # 数据库对象
 migrate = Migrate(app, db)  # 迁移引擎对象
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 from app import routes, models, errors
